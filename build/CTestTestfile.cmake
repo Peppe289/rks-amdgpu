ddd2ee86683eb28eb1d601d99b06a32dd6fde04a
Peppe289 <gsperanza204@gmail.com>
2023-08-22 17:43:45 +0200

    clean up
    Signed-off-by: Peppe289 <gsperanza204@gmail.com>

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/peppe289/Desktop/Kernel_Monitor/system_amd
-# Build directory: /home/peppe289/Desktop/Kernel_Monitor/system_amd/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
